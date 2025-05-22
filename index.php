c081b74cf811038b2c87e4604f74442d6e97795b
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-05-21 22:20:53 -0400

    Completed header and footer

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
     <link rel="stylesheet" type="txt/css" href="css/main.css"/>
 </head>
 <body>
+    <?php include ("header.php");  ?>
+
+    <main>
+        <h2>Students List</h2>
+    </main>
+
+    <?php include ("footer.php"); ?>
     
 </body>
 </html>
